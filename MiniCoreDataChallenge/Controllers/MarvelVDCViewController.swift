88703564083d7ef67f33c8e7943ae201d7724e66
Noah Pope <npope@tutanota.com>
2023-07-04 11:18:37 -0400

    Initial Commit

---
+++ MiniCoreDataChallenge/Controllers/MarvelVDCViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MiniCoreDataChallenge
+//
+//  Created by Noah Pope on 7/3/23.
+//
+
+import UIKit
+import CoreData
+
+
+class MarvelVDCViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
