f9c14b9445076f98db13964bdf6f052b10979742
Noah Pope <npope@tutanota.com>
2023-07-05 07:10:46 -0400

    Changed DataModel name and got root VC to read the Universe entity in core data

--- MiniCoreDataChallenge/Controllers/MarvelVDCViewController.swift
+++ MiniCoreDataChallenge/Controllers/MarvelVDCViewController.swift
@@ -16,10 +16,10 @@ class MarvelVDCViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
     }
 
 
 }
 
-//initial commit
+
