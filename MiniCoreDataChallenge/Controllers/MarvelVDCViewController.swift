898f7bfcfc9be8508d3873d63887e6dad1fb912e
Noah Pope <npope@tutanota.com>
2023-07-07 07:43:28 -0400

    loading cells and setting up add button functionality

--- MiniCoreDataChallenge/Controllers/MarvelVDCViewController.swift
+++ MiniCoreDataChallenge/Controllers/MarvelVDCViewController.swift
@@ -9,21 +9,48 @@ import UIKit
 import CoreData
 
 
-class MarvelVDCViewController: UIViewController {
+class MarvelVDCViewController: UITableViewController {
     
-    //commit test 4
     var MultiVerse = [Universe]()
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
         print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
-        print("testing")
+        
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithTransparentBackground()
+        appearance.backgroundColor = UIColor.systemBlue
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        navigationItem.standardAppearance = appearance
+        navigationItem.scrollEdgeAppearance = appearance
     }
+    
+// MARK: - DataSource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UniverseCell", for: indexPath)
+        
+        cell.textLabel?.text = "testing10"
+        
+        return cell
+    }
+    
+// MARK: - Delegate Methods
 
+// MARK: - Data Maniulation Methods
+    //save and load
+    
+// MARK: - Add new items
 
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
 }
 
-//commit testing after successful github update 2
 
 
